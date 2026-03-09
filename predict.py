4533172b93d3a73392dc51349cc56f27132a2618
SanjanaGinjala <sanjanaginjala@gmail.com>
2026-03-09 17:44:09 +0530

    updated predictions

--- predict.py
+++ predict.py
@@ -5,15 +5,27 @@ heart_model = joblib.load("models/heart_model.pkl")
 diabetes_model = joblib.load("models/diabetes_model.pkl")
 parkinsons_model = joblib.load("models/parkinsons_model.pkl")
 
+
+def prepare_input(data, model):
+    expected = model.n_features_in_
+
+    # fill missing values with 0
+    if len(data) < expected:
+        data = data + [0]*(expected-len(data))
+
+    return np.array(data).reshape(1,-1)
+
+
 def predict_heart(data):
-    data = np.array(data).reshape(1,-1)
+    data = prepare_input(data, heart_model)
     return heart_model.predict(data)
 
+
 def predict_diabetes(data):
-    print("Expected features:", diabetes_model.n_features_in_)
-    data = np.array(data).reshape(1,-1)
+    data = prepare_input(data, diabetes_model)
     return diabetes_model.predict(data)
-    
+
+
 def predict_parkinsons(data):
-    data = np.array(data).reshape(1,-1)
+    data = prepare_input(data, parkinsons_model)
     return parkinsons_model.predict(data)
